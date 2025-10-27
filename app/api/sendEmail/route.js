4d670b3b238cea9c2576696c8864811d6215717e
Kachi <onyekansirimobi16@gmail.com>
2025-10-27 11:10:00 +0100

    Correction

--- app/api/sendEmail/route.js
+++ app/api/sendEmail/route.js
@@ -6,7 +6,6 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req) {
   try {
-    // console.log("✅ POST request received at /api/sendEmail");
 
     const body = await req.json();
     console.log("📩 Incoming data:", body);
@@ -15,9 +14,8 @@ export async function POST(req) {
 
     const data = await resend.emails.send({
       from: "Pinpoints Nigeria Limited <noreply@pinpointsng.com>", 
-      to: "lynnradney4@gmail.com",  //aloziena@yahoo.co.uk where you want to receive messages
+      to: "aloziena@yahoo.co.uk",  // where you want to receive messages
       subject: `New Inquiry from ${name || "Unknown"}`,
-      // reply_to: email,
       html: `
         <h3>New Inquiry Received</h3>
         <p><strong>Name:</strong> ${name}</p>
@@ -35,9 +33,7 @@ export async function POST(req) {
       subject: "Your Application Has Been Received",
       html: `
         <h2>Hello ${name},</h2>
-        <p>Thank you for your interest in the <strong>Debt Management Consultant Training Programme</strong>.</p>
-        <p>We have received your application successfully and our team will review your submission shortly.</p>
-        <p>For any inquiries, please contact us at <a href="mailto:info@pinpointsng.com">info@pinpointsng.com</a>.</p>
+        <p>Thank you for contacting us! We’ve received your inquiry and our team will get back to you shortly.</p>
         <p>Best regards,<br><strong>The Pinpoints Team</strong></p>
       `,
     });
